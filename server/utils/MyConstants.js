ed057a710ad1f5f8438214f4a935144aac61139c
Vinbobo <128805837+Vinbobo@users.noreply.github.com>
2024-09-03 09:11:26 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,8 +1,8 @@
 const MyConstants = {
-    DB_SERVER:'clustermern.qages5h.mongodb.net',
-    DB_USER: 'new-user0',
-    DB_PASS: 'Abc123456',
-    DB_DATABASE: 'shoppingonline',
+    DB_SERVER:'newdatabase.cq8p7.mongodb.net',
+    DB_USER: 'admin',
+    DB_PASS: 'WdMAA4kYS3-7wSi',
+    DB_DATABASE: 'nhathuocvanchuyen',
     JWT_SECRET: 'TranKhanhVinh', //nhap j cx dc
     JWT_EXPIRES: '315576000', //milisecond
     EMAIL_USER: 'banhbaobeo2205@gmail.com',
